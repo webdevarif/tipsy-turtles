ccdc26c70d7d55155f488784d8e8cb31872e276a
Arif Hossin || Full Stack Developer <46327525+webdevarif@users.noreply.github.com>
2024-01-31 01:30:31 +0600

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,23 +1,44 @@
 import { motion } from "framer-motion";
+import { useEffect } from 'react';
 import Header from "../Header";
 import Footer from "../Footer";
+import { useRouter } from 'next/router';
 
-const Layout = ({ children, footer=true }) => (
-  <div className="overflow-hidden">
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping: 20,
-      }}
-    >
-      <Header />
-      {children}
-      { footer && <Footer />}
-    </motion.div>
-  </div>
-);
+const Layout = ({ children, footer = true }) => {
+  // Get the router object
+  const router = useRouter();
+
+  // Access the hash value
+  const hashValue = router.asPath.split('#')[1];
+
+  useEffect(() => {
+    // Scroll to the element with the matching ID when the component mounts
+    if (hashValue) {
+      const targetElement = document.getElementById(hashValue);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [hashValue]);
+
+  return (
+    <div className="overflow-hidden">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+      >
+        <Header />
+        {children}
+        {footer && <Footer />}
+      </motion.div>
+    </div>
+  );
+};
+
 export default Layout;
